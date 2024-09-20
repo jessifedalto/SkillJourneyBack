88d8d2e7a0da81dcb1ef49c09f51fcfbdad65f7e
jessifedalto <jessicafedalto.jf@gmail.com>
2024-09-20 09:20:25 -0300

    feat: resolved problem

--- src/routes/EmployeeSkill.js
+++ src/routes/EmployeeSkill.js
@@ -5,7 +5,6 @@ const route = express.Router();
 
 route 
     .patch('/update/:id', EmployeeSkillController.update)
-    .delete('/delete/:id', EmployeeSkillController.delete)
     .get('/employee/:id', EmployeeSkillController.getByEmployee)
     .get('/skill/:id', EmployeeSkillController.getBySkill)
     .post('/create/:employeeId/:skillId', EmployeeSkillController.create)
