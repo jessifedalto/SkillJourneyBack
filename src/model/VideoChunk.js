d80dd31440c43679b1b91db62d70de48316c0e59
YasminTrembulack <yasmin.agostinho.012@gmail.com>
2024-11-19 10:22:46 -0300

    feat: remove models video and videochunk

--- src/model/VideoChunk.js
+++ src/model/VideoChunk.js
@@ -1,42 +1,42 @@
-import { DataTypes, Model } from 'sequelize';
-import sequelizeInstance from '../../startup/db.js';
-import Video from './Video.js'
+// import { DataTypes, Model } from 'sequelize';
+// import sequelizeInstance from '../../startup/db.js';
+// import Video from './Video.js'
 
-class VideoChunk extends Model {}
+// class VideoChunk extends Model {}
 
-VideoChunk.init({
-    id: {
-        type: DataTypes.UUID,           // Define o tipo UUID para o ID
-        defaultValue: DataTypes.UUIDV4, // Gera um UUID v4 por padrão
-        primaryKey: true                // Define como chave primária
-    },
-    videoId: {
-        type: DataTypes.UUID,
-        allowNull: false,
-        references: {
-            model: 'tb_video',
-            key: 'id'
-        }
-    },
-    chunck_number: {
-        type: DataTypes.INTEGER,
-        allowNull: false
-    },
-    data: {
-        type: DataTypes.BLOB,
-        allowNull: false
-    }
-}, {
-    sequelize: sequelizeInstance,      // Passa a instância do Sequelize
-    modelName: 'VideoChunk',      // Nome do modelo
-    tableName: 'tb_videochunk', // Nome da tabela no banco de dados
-    timestamps: true, // Adiciona `createdAt` e `updatedAt`
-    paranoid: true // Adiciona `deletedAt` para suporte a soft deletes
-});
-
-// VideoChunk.belongsTo(Video, {
-//     foreignKey: 'videoId',
-//     onDelete: 'CASCADE'
+// VideoChunk.init({
+//     id: {
+//         type: DataTypes.UUID,           // Define o tipo UUID para o ID
+//         defaultValue: DataTypes.UUIDV4, // Gera um UUID v4 por padrão
+//         primaryKey: true                // Define como chave primária
+//     },
+//     videoId: {
+//         type: DataTypes.UUID,
+//         allowNull: false,
+//         references: {
+//             model: 'tb_video',
+//             key: 'id'
+//         }
+//     },
+//     chunck_number: {
+//         type: DataTypes.INTEGER,
+//         allowNull: false
+//     },
+//     data: {
+//         type: DataTypes.BLOB,
+//         allowNull: false
+//     }
+// }, {
+//     sequelize: sequelizeInstance,      // Passa a instância do Sequelize
+//     modelName: 'VideoChunk',      // Nome do modelo
+//     tableName: 'tb_videochunk', // Nome da tabela no banco de dados
+//     timestamps: true, // Adiciona `createdAt` e `updatedAt`
+//     paranoid: true // Adiciona `deletedAt` para suporte a soft deletes
 // });
 
-export default VideoChunk;
+// // VideoChunk.belongsTo(Video, {
+// //     foreignKey: 'videoId',
+// //     onDelete: 'CASCADE'
+// // });
+
+// export default VideoChunk;
