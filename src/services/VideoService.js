dcc67a95c3585245741f6636c290932f693767bd
YasminTrembulack <yasmin.agostinho.012@gmail.com>
2024-11-19 10:31:16 -0300

    a

--- src/services/VideoService.js
+++ src/services/VideoService.js
@@ -1,26 +1,26 @@
 
-import Video from '../model/Video.js';
+// import Video from '../model/Video.js';
 
 export default class VideoService {
     static async getChunksByVideoId(videoId) {
-        try {
-            const video = await Video.findByPk(videoId, {
-                include: [{
-                    model: 'tb_videochunk',
-                    as: 'chunks'
-                }]
-            });
+        // try {
+        //     const video = await Video.findByPk(videoId, {
+        //         include: [{
+        //             model: 'tb_videochunk',
+        //             as: 'chunks'
+        //         }]
+        //     });
 
-            return video;
-        } catch (error) {
-            throw new Error('Erro ao buscar chunks de vídeo: ' + error.message);
-        }
+        //     return video;
+        // } catch (error) {
+        //     throw new Error('Erro ao buscar chunks de vídeo: ' + error.message);
+        // }
     }
 
     static async getByContent(contentId){
 
-        const videos = await Video.findAll({ where: { trainingContentId: contentId } });
-        return videos;
+        // const videos = await Video.findAll({ where: { trainingContentId: contentId } });
+        // return videos;
 
     }
 }
