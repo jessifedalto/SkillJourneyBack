b4235e808fb146caf21277b99a7d3752968a001c
YasminTrembulack <yasmin.agostinho.012@gmail.com>
2024-11-19 10:42:00 -0300

    feat: change query from video service

--- src/services/VideoService.js
+++ src/services/VideoService.js
@@ -5,12 +5,19 @@ import sequelizeInstance from '../../startup/db.js';
 export default class VideoService {
     static async getChunksByVideoId(videoId) {
         try {
-            const [results, metadata] = await sequelizeInstance.query(`SELECT * FROM tb_video WHERE id = ${videoId}`);
-
+            const [results, metadata] = await sequelizeInstance.query(
+                'SELECT * FROM tb_video WHERE id = :videoId',
+                {
+                    replacements: { videoId }, // substitui :videoId pelo valor da variável videoId
+                    type: sequelizeInstance.QueryTypes.SELECT // garante que o resultado será um array de resultados
+                }
+            );
+        
             return results;
         } catch (error) {
             throw new Error('Erro ao buscar chunks de vídeo: ' + error.message);
         }
+        
     }
 
     static async getByContent(contentId){
