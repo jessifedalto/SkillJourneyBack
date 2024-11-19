f6f1289ebf52405440f404fde5a713138fb4d3f8
YasminTrembulack <yasmin.agostinho.012@gmail.com>
2024-11-19 10:38:17 -0300

    a

--- src/services/VideoService.js
+++ src/services/VideoService.js
@@ -1,23 +1,22 @@
 
 // import Video from '../model/Video.js';
+import sequelizeInstance from '../../startup/db.js';
 
 export default class VideoService {
     static async getChunksByVideoId(videoId) {
-        // try {
-        //     const video = await Video.findByPk(videoId, {
-        //         include: [{
-        //             model: 'tb_videochunk',
-        //             as: 'chunks'
-        //         }]
-        //     });
+        try {
+            const [results, metadata] = await sequelizeInstance.query(`SELECT * FROM tb_video WHERE id = ${videoId}`);
 
-        //     return video;
-        // } catch (error) {
-        //     throw new Error('Erro ao buscar chunks de vídeo: ' + error.message);
-        // }
+            return results;
+        } catch (error) {
+            throw new Error('Erro ao buscar chunks de vídeo: ' + error.message);
+        }
     }
 
     static async getByContent(contentId){
+        const [results, metadata] = await sequelizeInstance.query(`SELECT * FROM tb_video WHERE TrainingContentId = ${contentId}`);
+
+        return results;
 
         // const videos = await Video.findAll({ where: { trainingContentId: contentId } });
         // return videos;
